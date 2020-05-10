f66095a3e782e13192f66cee3c3eb1646863d0d5
Mathéo _430 <WeBuildHub@users.noreply.github.com>
2020-05-10 23:36:53 +0200

    0.0.005
    []: Facilite la synthaxe pour les fonctions comme ChangeStats.

--- PyApp Sys/Program.py
+++ PyApp Sys/Program.py
@@ -1,3 +1,3 @@
 import pya
 #Programme éditable, à coder et exécuter vous-même#
-pya.ChangeStats("logs.log", "rajouter", "Ouais, bienvenue à tous sur ce site qui consiste à vous aider à créer votre site internet.")
+pya.ChangeStats("logs.log", "rajouter", "J'écris un log !")
